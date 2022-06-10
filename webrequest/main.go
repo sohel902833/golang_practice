d876eec60cf7c1f89081f95ac9de4156757bd006
sohel902833 <mdsohrabhossainsohel902802@gmail.com>
2022-06-10 09:47:27 +0600

    added post request using application json

--- webrequest/main.go
+++ webrequest/main.go
@@ -4,12 +4,17 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 const url = "https://www.youtube.com/"
 
 func main() {
 	fmt.Println("Welcome to LCO web request")
+	performPostJsonRequest()
+}
+
+func performGetRequest() {
 	response, err := http.Get(url)
 
 	if err != nil {
@@ -24,5 +29,29 @@ func main() {
 	}
 	content := string(databytes)
 	fmt.Println(content)
+}
+
+func performPostJsonRequest() {
+	const myUrl = "https://jsonplaceholder.typicode.com/posts"
+
+	requestBody := strings.NewReader(`
+		{
+			"name":"Md Sohrab Hossain Sohel",
+			"roll":902833,
+			"platform":"SohelCodeOnline.in"
+		}
+	`)
+
+	response, err := http.Post(myUrl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer response.Body.Close()
+	content, _ := ioutil.ReadAll(response.Body)
+
+	fmt.Println(string(content))
+
+}
+func performPostFormRequest() {
 
 }
